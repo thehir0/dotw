0df1ad8af19ddab711c7d357052d09af29848ddf
damirabdulaev <spedefire@mail.ru>
2022-06-18 15:42:00 +0300

    little fix

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,8 @@ class MainMenu extends StatefulWidget {
   State<MainMenu> createState() => _MainMenuState();
 }
 
+}
+
 class _MainMenuState extends State<MainMenu> {
   @override
   Widget build(BuildContext context) {

af6b2261cc6a31a65853e35d9dceb2ec216f1024
hir0github <almazdautov1234523@gmail.com>
2022-07-09 01:38:21 +0300

    leaderboard bug fix

--- lib/widgets/leaderboard.dart
+++ lib/widgets/leaderboard.dart
@@ -118,7 +118,7 @@ class _LeaderboardState extends State<Leaderboard> {
                             children: [
                               /**TITLE**/
                               Container(
-                                margin: const EdgeInsets.only(top: 10),
+                                margin: const EdgeInsets.only(top: 20),
                                 child: Row(
                                   crossAxisAlignment: CrossAxisAlignment.end,
                                   children: [
